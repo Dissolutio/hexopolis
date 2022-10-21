3e2b0941b90f814697abc81adb43c5940931be34
John <entity.john@gmail.com>
2022-10-21 12:05:43 -0500

    roll in new bgio-contexts, then we will strangle out old usages

--- src/hexed-meadow-ui/HexedMeadowBoard.tsx
+++ src/hexed-meadow-ui/HexedMeadowBoard.tsx
@@ -16,38 +16,92 @@ import { Controls } from './controls'
 import { MapDisplay } from './hexmap'
 import { theme } from './theme'
 import './theme.css'
+import {
+  BgioClientInfoProvider,
+  BgioGProvider,
+  BgioMovesProvider,
+  BgioEventsProvider,
+  BgioCtxProvider,
+  BgioChatProvider,
+} from 'bgio-contexts'
+import { ChatMessage } from 'boardgame.io'
 
-export const HexedMeadowBoard: React.FunctionComponent<BoardProps> = ({
-  playerID,
+type MyBoardProps = BoardProps & { chatMessages?: ChatMessage[] }
+
+export const HexedMeadowBoard = ({
+  // G
   G,
+  // CTX
   ctx,
+  // MOVES
   moves,
   undo,
   redo,
-}) => {
+  // EVENTS
+  events,
+  reset,
+  // CHAT
+  sendChatMessage,
+  chatMessages = [],
+  // ALSO ON BOARD PROPS
+  playerID,
+  log,
+  matchID,
+  matchData,
+  isActive,
+  isMultiplayer,
+  isConnected,
+  credentials,
+}: MyBoardProps) => {
   return (
     <ThemeProvider theme={theme(playerID ?? '')}>
-      <PlayerIDProvider playerID={playerID ?? ''}>
-        <GProvider G={G}>
-          <CtxProvider ctx={ctx}>
-            <MovesProvider moves={moves} undo={undo} redo={redo}>
-              <MapContextProvider>
-                <UIContextProvider>
-                  <PlacementContextProvider>
-                    <PlayContextProvider>
-                      <Layout>
-                        <HeaderNav />
-                        <MapDisplay />
-                        <Controls />
-                      </Layout>
-                    </PlayContextProvider>
-                  </PlacementContextProvider>
-                </UIContextProvider>
-              </MapContextProvider>
-            </MovesProvider>
-          </CtxProvider>
-        </GProvider>
-      </PlayerIDProvider>
+      <BgioClientInfoProvider
+        log={log}
+        playerID={playerID || ''}
+        matchID={matchID}
+        matchData={matchData}
+        credentials={credentials || ''}
+        isMultiplayer={isMultiplayer}
+        isConnected={isConnected}
+        isActive={isActive}
+      >
+        <BgioGProvider G={G}>
+          <BgioCtxProvider ctx={ctx}>
+            <BgioMovesProvider moves={moves} undo={undo} redo={redo}>
+              <BgioEventsProvider reset={reset} events={events}>
+                <BgioChatProvider
+                  chatMessages={chatMessages}
+                  sendChatMessage={sendChatMessage}
+                >
+                  {/* START OLD ONES */}
+                  <PlayerIDProvider playerID={playerID ?? ''}>
+                    <GProvider G={G}>
+                      <CtxProvider ctx={ctx}>
+                        <MovesProvider moves={moves} undo={undo} redo={redo}>
+                          <MapContextProvider>
+                            <UIContextProvider>
+                              <PlacementContextProvider>
+                                <PlayContextProvider>
+                                  <Layout>
+                                    <HeaderNav />
+                                    <MapDisplay />
+                                    <Controls />
+                                  </Layout>
+                                </PlayContextProvider>
+                              </PlacementContextProvider>
+                            </UIContextProvider>
+                          </MapContextProvider>
+                        </MovesProvider>
+                      </CtxProvider>
+                    </GProvider>
+                  </PlayerIDProvider>
+                  {/* END OLD ONES */}
+                </BgioChatProvider>
+              </BgioEventsProvider>
+            </BgioMovesProvider>
+          </BgioCtxProvider>
+        </BgioGProvider>
+      </BgioClientInfoProvider>
     </ThemeProvider>
   )
 }
