26c98aee262b3a12e6bb0dafb171671d2075a08f
John Moen <entity.john@gmail.com>
2022-11-28 12:04:09 -0600

    update icons to include new units

--- src/hexed-meadow-ui/unit-icons/UnitIcon.tsx
+++ src/hexed-meadow-ui/unit-icons/UnitIcon.tsx
@@ -1,6 +1,12 @@
 import { PlacementUnit } from 'game/types'
 import React from 'react'
-import { GiDiabloSkull, GiNinjaArmor, GiMissileMech } from 'react-icons/gi'
+import {
+  GiDiabloSkull,
+  GiNinjaArmor,
+  GiMissileMech,
+  GiAlienStare,
+  GiAlliedStar,
+} from 'react-icons/gi'
 
 const playerIconColors: { [key: string]: string } = {
   '0': 'var(--bee-yellow)',
@@ -39,11 +45,20 @@ export const UnitIcon = ({
 
   switch (armyCardID) {
     case 'hs1000':
+      // marro warriors
       return <GiDiabloSkull {...gameIconProps} />
     case 'hs1001':
+      // deathwalker 9000
       return <GiMissileMech {...gameIconProps} />
     case 'hs1002':
+      // izumi samurai
       return <GiNinjaArmor {...gameIconProps} />
+    case 'hs1003':
+      // sgt drake
+      return <GiAlliedStar {...gameIconProps} />
+    case 'hs1014':
+      // negoksa
+      return <GiAlienStare {...gameIconProps} />
     default:
       return null
   }
