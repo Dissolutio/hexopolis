85ad8a7139a05a4e77695cb15c8c199d88fb1ce3
John Moen <entity.john@gmail.com>
2022-12-02 18:18:55 -0600

    show how undoable is on long form move

--- src/game/move-action.ts
+++ src/game/move-action.ts
@@ -8,54 +8,55 @@ import {
 } from './selectors'
 import { BoardHex, BoardHexes, GameState, GameUnit, GameUnits } from './types'
 
-export const moveAction: Move<GameState> = (
-  { G, ctx },
-  unit: GameUnit,
-  endHex: BoardHex
-) => {
-  const { unitID, movePoints } = unit
-  const playersOrderMarkers = G.players[ctx.currentPlayer].orderMarkers
-  const endHexID = endHex.id
-  const startHex = selectHexForUnit(unitID, G.boardHexes)
-  const startHexID = startHex?.id ?? ''
-  const currentMoveRange = G.gameUnits[unitID].moveRange
-  const isInSafeMoveRange = currentMoveRange.safe.includes(endHexID)
-  const moveCost = HexUtils.distance(startHex as Hex, endHex)
-  // clone G
-  const newBoardHexes: BoardHexes = { ...G.boardHexes }
-  const newGameUnits: GameUnits = { ...G.gameUnits }
-  // update moved units counter
-  const unitsMoved = [...G.unitsMoved]
-  if (!unitsMoved.includes(unitID)) {
-    unitsMoved.push(unitID)
-    G.unitsMoved = unitsMoved
-  }
-  // update unit position
-  newBoardHexes[startHexID].occupyingUnitID = ''
-  newBoardHexes[endHexID].occupyingUnitID = unitID
-  // update unit move-points
-  const newMovePoints = movePoints - moveCost
-  newGameUnits[unitID].movePoints = newMovePoints
-  // update move-ranges for this turn's units
-  const unrevealedGameCard = selectUnrevealedGameCard(
-    playersOrderMarkers,
-    G.armyCards,
-    G.currentOrderMarker
-  )
-  const currentTurnUnits = selectUnitsForCard(
-    unrevealedGameCard?.gameCardID ?? '',
-    G.gameUnits
-  )
+export const moveAction: Move<GameState> = {
+  undoable: ({ G, ctx }) => {
+    return false
+  },
+  move: ({ G, ctx }, unit: GameUnit, endHex: BoardHex) => {
+    const { unitID, movePoints } = unit
+    const playersOrderMarkers = G.players[ctx.currentPlayer].orderMarkers
+    const endHexID = endHex.id
+    const startHex = selectHexForUnit(unitID, G.boardHexes)
+    const startHexID = startHex?.id ?? ''
+    const currentMoveRange = G.gameUnits[unitID].moveRange
+    const isInSafeMoveRange = currentMoveRange.safe.includes(endHexID)
+    const moveCost = HexUtils.distance(startHex as Hex, endHex)
+    // clone G
+    const newBoardHexes: BoardHexes = { ...G.boardHexes }
+    const newGameUnits: GameUnits = { ...G.gameUnits }
+    // update moved units counter
+    const unitsMoved = [...G.unitsMoved]
+    if (!unitsMoved.includes(unitID)) {
+      unitsMoved.push(unitID)
+      G.unitsMoved = unitsMoved
+    }
+    // update unit position
+    newBoardHexes[startHexID].occupyingUnitID = ''
+    newBoardHexes[endHexID].occupyingUnitID = unitID
+    // update unit move-points
+    const newMovePoints = movePoints - moveCost
+    newGameUnits[unitID].movePoints = newMovePoints
+    // update move-ranges for this turn's units
+    const unrevealedGameCard = selectUnrevealedGameCard(
+      playersOrderMarkers,
+      G.armyCards,
+      G.currentOrderMarker
+    )
+    const currentTurnUnits = selectUnitsForCard(
+      unrevealedGameCard?.gameCardID ?? '',
+      G.gameUnits
+    )
 
-  currentTurnUnits.forEach((unit: GameUnit) => {
-    const { unitID } = unit
-    const moveRange = calcUnitMoveRange(unit, newBoardHexes, newGameUnits)
-    newGameUnits[unitID].moveRange = moveRange
-  })
-  // Make the move
-  if (isInSafeMoveRange) {
-    G.boardHexes = { ...newBoardHexes }
-    G.gameUnits = { ...newGameUnits }
-  }
-  return G
+    currentTurnUnits.forEach((unit: GameUnit) => {
+      const { unitID } = unit
+      const moveRange = calcUnitMoveRange(unit, newBoardHexes, newGameUnits)
+      newGameUnits[unitID].moveRange = moveRange
+    })
+    // Make the move
+    if (isInSafeMoveRange) {
+      G.boardHexes = { ...newBoardHexes }
+      G.gameUnits = { ...newGameUnits }
+    }
+    return G
+  },
 }
