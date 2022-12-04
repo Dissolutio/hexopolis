c3c09aab6016dfd147b5ea5cc9547276d500145a
John Moen <entity.john@gmail.com>
2022-12-04 14:04:25 -0600

    add round begin notification
    Need to build out reporting from initiative roll in order to be able to show it though

--- src/game/gamelog.ts
+++ src/game/gamelog.ts
@@ -1,5 +1,5 @@
 export type GameLogMessage = {
-  type: string // 'move' 'attack'
+  type: string // 'roundBegin' 'attack'
   id: string // round1:orderMarker1:unit1:attack3 => r1:om1:p1u1_hs1008:a3
   // attack related ones below
   unitID?: string
@@ -12,21 +12,29 @@ export type GameLogMessage = {
   shields?: number
   wounds?: number
   isFatal?: boolean
+  // roundBegin related ones below
 }
+export const gameLogTypes = {
+  move: 'move',
+  attack: 'attack',
+  roundBegin: 'roundBegin',
+}
+
 export type GameLogMessageDecoded = GameLogMessage & {
   msg: string
 }
 
 export const encodeGameLogMessage = (gameLog: GameLogMessage): string => {
   try {
-    switch (gameLog.type) {
-      case 'attack':
-        return JSON.stringify(gameLog)
-      case 'move':
-        return ''
-      default:
-        return ''
-    }
+    return JSON.stringify(gameLog)
+    // switch (gameLog.type) {
+    //   case 'attack':
+    //     return JSON.stringify(gameLog)
+    //   case 'roundBegin':
+    //     return JSON.stringify(gameLog)
+    //   default:
+    //     return ''
+    // }
   } catch (error) {
     console.error('🚀 ~ file: gamelog.ts ~ encodeGameLogMessage ~ error', error)
     return ''
@@ -51,22 +59,50 @@ export const decodeGameLogMessage = (
       wounds,
       isFatal,
     } = gameLog
-    const gameLogHumanFriendly = `${unitName} attacked ${defenderUnitName} for ${wounds} wounds (${skulls} skulls, ${shields} shields)`
-    return {
-      type,
-      id,
-      unitID,
-      unitName,
-      targetHexID,
-      defenderUnitName,
-      attackRolled,
-      defenseRolled,
-      skulls,
-      shields,
-      wounds,
-      isFatal,
-      msg: gameLogHumanFriendly,
+    switch (type) {
+      case gameLogTypes.attack:
+        const attackMsgText = `${unitName} attacked ${defenderUnitName} for ${wounds} wounds (${skulls} skulls, ${shields} shields)`
+        return {
+          type,
+          id,
+          unitID,
+          unitName,
+          targetHexID,
+          defenderUnitName,
+          attackRolled,
+          defenseRolled,
+          skulls,
+          shields,
+          wounds,
+          isFatal,
+          msg: attackMsgText,
+        }
+      case gameLogTypes.roundBegin:
+        const roundBeginMsgText = `Round ${id} has begun!`
+        return {
+          type,
+          id, // id is the round number, for roundBegin log types
+          msg: roundBeginMsgText,
+        }
+      default:
+        break
     }
+    // const gameLogHumanFriendly = `${unitName} attacked ${defenderUnitName} for ${wounds} wounds (${skulls} skulls, ${shields} shields)`
+    // return {
+    //   type,
+    //   id,
+    //   unitID,
+    //   unitName,
+    //   targetHexID,
+    //   defenderUnitName,
+    //   attackRolled,
+    //   defenseRolled,
+    //   skulls,
+    //   shields,
+    //   wounds,
+    //   isFatal,
+    //   msg: gameLogHumanFriendly,
+    // }
   } catch (error) {
     console.error('🚀 ~ file: gamelog.ts ~ decodeGameLogMessage ~ error', error)
     return undefined
