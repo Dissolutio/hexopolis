f46ef01571847551c7f2e6d563e69fe2b3ee37f9
John Moen <entity.john@gmail.com>
2023-01-20 13:13:46 -0600

    simplify state, zoom just changes width & height

--- src/hexopolis-ui/hexmap/MapDisplay.tsx
+++ src/hexopolis-ui/hexmap/MapDisplay.tsx
@@ -15,39 +15,14 @@ type Props = {
 
 export const MapDisplay = ({ mapWrapperRef }: Props) => {
   const {
-    hexMap: { hexSize, flat, mapShape, mapId, mapSize },
+    hexMap: { hexSize, flat, mapId, mapSize },
   } = useBgioG()
   const { viewBox } = useMapContext()
   //! MAP SETUP/LAYOUT CONFIG
-  const isHexagonShapedMap = mapShape === MapShapes.hexagon
-  const isRectangleShapedMap = mapShape === MapShapes.rectangle
-  const hexagonalMapState = {
+  const initialMapState = {
     width: 100,
     height: 100,
-    origin: { x: 0, y: 0 },
-    flat,
-    spacing: 0.99,
   }
-  const rectangularMapState = {
-    width: 100,
-    height: 100,
-    origin: { x: -750, y: -500 },
-    flat,
-    spacing: 0.99,
-  }
-  // this works ok if the longer number is the length, and around 25 length/width, otherwise, it's off screen easily
-  const orientedRectangularMapState = {
-    width: 100,
-    height: 100,
-    origin: { x: -750, y: -500 },
-    flat,
-    spacing: 0.99,
-  }
-  const initialMapState = isHexagonShapedMap
-    ? hexagonalMapState
-    : isRectangleShapedMap
-    ? rectangularMapState
-    : orientedRectangularMapState
   const [mapState, setMapState] = React.useState(() => initialMapState)
   const recalculateMapState = () => setMapState(initialMapState)
   useEffect(() => {
@@ -76,6 +51,7 @@ export const MapDisplay = ({ mapWrapperRef }: Props) => {
       })
   }
   const handleClickZoomOut = () => {
+    // Early return if already all the way zoomed out
     // decreases width and height by zoom interval, attempts scroll correction afterwards
     const el = mapWrapperRef.current
     const currentScrollTop = el?.scrollTop ?? mapState.height - zoomInterval / 2
