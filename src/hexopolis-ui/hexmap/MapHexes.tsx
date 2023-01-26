8fbad0dbc9170fe6a83f55aeecca1919ffb33c21
John Moen <entity.john@gmail.com>
2023-01-26 11:56:58 -0600

    change hex text display, make some comment toggles

--- src/hexopolis-ui/hexmap/MapHexes.tsx
+++ src/hexopolis-ui/hexmap/MapHexes.tsx
@@ -141,8 +141,10 @@ export const MapHexes = ({ hexSize }: MapHexesProps) => {
             </AnimatePresence>
             <HexIDText
               hexSize={hexSize}
-              text={`${hex.altitude}`}
-              textLine2={`${unitName}`}
+              text={`${hex.id}`}
+              textLine2={`${hex.altitude}`}
+              // text={`${hex.altitude}`}
+              // textLine2={`${unitName}`}
             />
           </g>
         </Hexagon>
