342cc9a7c9b148d845725d4e947e562b97a4e110
John Moen <entity.john@gmail.com>
2023-02-07 15:01:19 -0600

    set stage back to attacking, or if we disen-died, end turn

--- src/game/moves/buff-card.ts
+++ src/game/moves/buff-card.ts
@@ -15,10 +15,38 @@ export const placeAttackSpirit: Move<GameState> = (
     )
   }
   G.gameArmyCards[indexToUpdate].attack++
-  const stageToRevertBackTo = G.isCurrentPlayerAttacking
-    ? stageNames.attacking
-    : stageNames.movement
+  const wasCurrentPlayerAttacking =
+    G.isCurrentPlayerAttacking === true ? true : false
   // clear isCurrentPlayerAttacking state after we use it
   G.isCurrentPlayerAttacking = false
-  events.setActivePlayers({ currentPlayer: stageToRevertBackTo })
+  if (wasCurrentPlayerAttacking) {
+    events.setActivePlayers({ currentPlayer: stageNames.attacking })
+  } else {
+    // we died from disengagement, can't move a dead man, and should end turn
+    events.endTurn()
+  }
+}
+export const placeArmorSpirit: Move<GameState> = (
+  { G, events },
+  { gameCardID }
+) => {
+  const indexToUpdate = G.gameArmyCards.findIndex(
+    (gc) => gc.gameCardID === gameCardID
+  )
+  if (indexToUpdate < 0) {
+    throw new Error(
+      'Placing armor spirit denied: gameCardID chosen not found in gameArmyCards'
+    )
+  }
+  G.gameArmyCards[indexToUpdate].defense++
+  const wasCurrentPlayerAttacking =
+    G.isCurrentPlayerAttacking === true ? true : false
+  // clear isCurrentPlayerAttacking state after we use it
+  G.isCurrentPlayerAttacking = false
+  if (wasCurrentPlayerAttacking) {
+    events.setActivePlayers({ currentPlayer: stageNames.attacking })
+  } else {
+    // we died from disengagement, can't move a dead man, and should end turn
+    events.endTurn()
+  }
 }
