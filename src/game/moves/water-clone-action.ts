bd2217b3eacdcd516f6f7b9454633af8f400533f
John Moen <entity.john@gmail.com>
2023-03-13 19:15:26 -0500

    roll for water clone should not be undoable

--- src/game/moves/water-clone-action.ts
+++ src/game/moves/water-clone-action.ts
@@ -1,77 +1,83 @@
 import type { Move } from 'boardgame.io'
+import { selectGlyphForHex } from 'game/selectors'
 import { encodeGameLogMessage, gameLogTypes } from '../gamelog'
 import { GameState, HexTerrain, UnitsCloning, WaterCloneRoll } from '../types'
+import { revealGlyph_G } from './G-mutators'
 
-export const rollForWaterClone: Move<GameState> = (
-  { G, ctx, random },
-  { unitsCloning, unitName }: { unitsCloning: UnitsCloning; unitName: string }
-) => {
-  const blankWaterCloneRoll = {
-    diceRolls: {},
-    threshholds: {},
-    cloneCount: 0,
-    placements: {},
-  }
-  const waterCloneRoll: WaterCloneRoll = unitsCloning.reduce(
-    (result, current) => {
-      const isOnWater =
-        G.boardHexes[current.clonerHexID].terrain === HexTerrain.water
-      const rollThreshhold = isOnWater ? 10 : 15
-      const roll = random.Die(20)
-      const isRollSuccessful = roll >= rollThreshhold
-      return {
-        ...result,
-        threshholds: {
-          ...result.threshholds,
-          [current.clonerID]: rollThreshhold,
-        },
-        diceRolls: {
-          ...result.diceRolls,
-          [current.clonerID]: roll,
-        },
-        placements: {
-          ...result.placements,
-          [current.clonerID]: isRollSuccessful
-            ? {
-                clonerID: current.clonerID,
-                clonerHexID: current.clonerHexID,
-                tails: current.tails,
-              }
-            : undefined,
-        },
-      }
-    },
-    blankWaterCloneRoll
-  )
-  // clear out the undefined values
-  Object.keys(waterCloneRoll?.placements ?? {}).forEach((keyOfPlacements) => {
-    if (!waterCloneRoll?.placements[keyOfPlacements]) {
-      delete waterCloneRoll?.placements[keyOfPlacements]
+export const rollForWaterClone: Move<GameState> = {
+  undoable: false,
+  move: (
+    { G, ctx, random },
+    { unitsCloning, unitName }: { unitsCloning: UnitsCloning; unitName: string }
+  ) => {
+    const blankWaterCloneRoll = {
+      diceRolls: {},
+      threshholds: {},
+      cloneCount: 0,
+      placements: {},
     }
-  })
-  const cloneCount = Object.values(waterCloneRoll?.placements ?? {}).length
-  waterCloneRoll.cloneCount = cloneCount
-  G.waterCloneRoll = waterCloneRoll
-  // add to game log
-  const rollsAndThreshholds = Object.keys(waterCloneRoll?.diceRolls ?? {}).map(
-    (gameUnitID) => {
+    const waterCloneRoll: WaterCloneRoll = unitsCloning.reduce(
+      (result, current) => {
+        const isOnWater =
+          G.boardHexes[current.clonerHexID].terrain === HexTerrain.water
+        const rollThreshhold = isOnWater ? 10 : 15
+        // const roll = random.Die(20)
+        const roll = 20
+        const isRollSuccessful = roll >= rollThreshhold
+        return {
+          ...result,
+          threshholds: {
+            ...result.threshholds,
+            [current.clonerID]: rollThreshhold,
+          },
+          diceRolls: {
+            ...result.diceRolls,
+            [current.clonerID]: roll,
+          },
+          placements: {
+            ...result.placements,
+            [current.clonerID]: isRollSuccessful
+              ? {
+                  clonerID: current.clonerID,
+                  clonerHexID: current.clonerHexID,
+                  tails: current.tails,
+                }
+              : undefined,
+          },
+        }
+      },
+      blankWaterCloneRoll
+    )
+    // clear out the undefined values
+    Object.keys(waterCloneRoll?.placements ?? {}).forEach((keyOfPlacements) => {
+      if (!waterCloneRoll?.placements[keyOfPlacements]) {
+        delete waterCloneRoll?.placements[keyOfPlacements]
+      }
+    })
+    const cloneCount = Object.values(waterCloneRoll?.placements ?? {}).length
+    waterCloneRoll.cloneCount = cloneCount
+    G.waterCloneRoll = waterCloneRoll
+    // add to game log
+    const rollsAndThreshholds = Object.keys(
+      waterCloneRoll?.diceRolls ?? {}
+    ).map((gameUnitID) => {
       return [
         waterCloneRoll.diceRolls[gameUnitID],
         waterCloneRoll.threshholds[gameUnitID],
       ]
-    }
-  )
-  const id = `r${G.currentRound}:om${G.currentOrderMarker}:waterClone`
-  const gameLogForThisMove = encodeGameLogMessage({
-    type: gameLogTypes.waterClone,
-    id,
-    playerID: ctx.currentPlayer,
-    rollsAndThreshholds,
-    cloneCount,
-    unitName,
-  })
-  G.gameLog.push(gameLogForThisMove)
-  //
+    })
+    const id = `r${G.currentRound}:om${G.currentOrderMarker}:waterClone`
+    const gameLogForThisMove = encodeGameLogMessage({
+      type: gameLogTypes.waterClone,
+      id,
+      playerID: ctx.currentPlayer,
+      rollsAndThreshholds,
+      cloneCount,
+      unitName,
+    })
+    G.gameLog.push(gameLogForThisMove)
+    //
+  },
 }
 export const finishWaterCloningAndEndTurn: Move<GameState> = ({
   G,
