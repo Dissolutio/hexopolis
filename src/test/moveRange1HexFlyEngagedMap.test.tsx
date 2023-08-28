df5629ccd2093590e1b20bab0923da9b799941cd
John M <entity.john@gmail.com>
2023-08-27 22:00:33 -0700

    add test for stealth flying

--- src/test/moveRange1HexFlyEngagedMap.test.tsx
+++ src/test/moveRange1HexFlyEngagedMap.test.tsx
@@ -39,39 +39,37 @@ describe('1-hex flying unit without stealth, starting engaged to enemies', () =>
   })
 })
 
-// describe('MOVE RANGE PASS THRU TESTS: test that a unit cannot move thru enemy units', () => {
-//   const makeGameState = () => {
-//     const numPlayers = 2
-//     const withPrePlacedUnits = true
-//     const withStealth = false
-//     return makeMoveRangePassThruScenario(
-//       withStealth,
-//       numPlayers,
-//       withPrePlacedUnits
-//     )
-//   }
-//   const gameState = makeGameState()
-//   // this test assumes there are two players, and each has one unit, so 2 unitIDs: p0u0,p1u1
-//   const unitMovingID = 'p1u1'
-//   const unitMoving = {
-//     ...gameState.gameUnits[unitMovingID],
-//     movePoints: 5,
-//   }
-//   const myMoveRange = computeUnitMoveRange({
-//     isFlying: false,
-//     isGrappleGun: false,
-//     hasMoved: false,
-//     unit: unitMoving,
-//     boardHexes: gameState.boardHexes,
-//     gameUnits: gameState.gameUnits,
-//     armyCards: gameState.gameArmyCards,
-//     glyphs: gameState.hexMap.glyphs,
-//   })
-//   test('moving thru enemy units should not be possible', () => {
-//     expect(
-//       myMoveRange[
-//         moveRange1HexFlyEngagedMapTestHexIDs.unreachableWithoutGhostWalk
-//       ]
-//     ).toBe(undefined)
-//   })
-// })
+describe('1-hex flying unit with stealth, starting engaged to 2 enemies, can only fly to adjacent hex if it disengages', () => {
+  const makeGameState = () => {
+    const numPlayers = 2
+    const withPrePlacedUnits = true
+    const withStealth = true
+    return makeMoveRange1HexFlyEngagedScenario(
+      withStealth,
+      numPlayers,
+      withPrePlacedUnits
+    )
+  }
+  const gameState = makeGameState()
+  // this test assumes there are two players, and p0 has 2 units while p1 has 1, so 3 unitIDs: p0u0,p0u1,p1u2
+  const unitMovingID = 'p1u2'
+  const unitMoving = {
+    ...gameState.gameUnits[unitMovingID],
+    movePoints: 5,
+  }
+  const myMoveRange = computeUnitMoveRange({
+    isFlying: true,
+    isGrappleGun: false,
+    hasMoved: false,
+    unit: unitMoving,
+    boardHexes: gameState.boardHexes,
+    gameUnits: gameState.gameUnits,
+    armyCards: gameState.gameArmyCards,
+    glyphs: gameState.hexMap.glyphs,
+  })
+  test('with stealth, 1-hex flying unit, already engaged to 2 units, can safely fly to adjacent hex', () => {
+    expect(
+      myMoveRange[moveRange1HexFlyEngagedMapTestHexIDs.adjacentHex]?.isSafe
+    ).toBe(true)
+  })
+})
