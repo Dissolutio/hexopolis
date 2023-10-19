177086c4faa0a75150d35d8702512bc9a84b3579
John M <entity.john@gmail.com>
2023-10-19 10:53:33 -0700

    style the float animation of selected unit

--- src/hexopolis-ui/world/hexmap3d/GameUnit3D.tsx
+++ src/hexopolis-ui/world/hexmap3d/GameUnit3D.tsx
@@ -80,9 +80,9 @@ const FloatSelectedWrapper = ({
     return (
       <Float
         speed={10} // Animation speed, defaults to 1
-        rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+        rotationIntensity={0.1} // XYZ rotation intensity, defaults to 1
         floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-        floatingRange={[0.2, 0.8]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+        floatingRange={[0.1, 0.3]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
       >
         {children}
       </Float>
