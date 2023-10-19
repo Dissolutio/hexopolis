1e6249be9fadc0da79a178532f8c8ba086331f01
John M <entity.john@gmail.com>
2023-10-19 10:55:25 -0700

    tidy

--- src/game/setup/scenarios.ts
+++ src/game/setup/scenarios.ts
@@ -51,18 +51,12 @@ const generatePlayerAndReadyAndOMStates = ({
 }) =>
   isDevOverrideState
     ? {
-        // Use the state below to start a game in the play-phase already
+        // Ready states can be edited to change what phase the game starts in (there's some state change too, like starting armies, unit placement, order markers)
         draftReady: generateReadyStateForNumPlayers(numPlayers, true),
-        placementReady: generateReadyStateForNumPlayers(numPlayers, false),
+        placementReady: generateReadyStateForNumPlayers(numPlayers, true),
         orderMarkersReady: generateReadyStateForNumPlayers(numPlayers, true),
         orderMarkers: generatePreplacedOrderMarkers(numPlayers, startingArmies),
         players: playersStateWithPrePlacedOMs(numPlayers, startingArmies),
-        // Use the state below for a From-the-draft-phase local 2-player game
-        // draftReady: generateReadyStateForNumPlayers(numPlayers, false),
-        // placementReady: generateReadyStateForNumPlayers(numPlayers, false),
-        // orderMarkersReady: generateReadyStateForNumPlayers(numPlayers, false),
-        // orderMarkers: generateBlankOrderMarkersForNumPlayers(numPlayers),
-        // players: generateBlankPlayersStateForNumPlayers(numPlayers),
         ...someInitialGameState,
       }
     : {
